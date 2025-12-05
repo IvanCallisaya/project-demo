b7ee012e6c99fa586196e2410032fa43d48c5f0d
Desarrollo Radiast <desarrolloradiast@gmail.com>
2025-12-05 11:05:47 -0400

    Corrigiendo importacion de estilos en guess y admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AdminLTE + Laravel 10</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body class="hold-transition sidebar-mini">

d91d6cd6ce5ffa87fa9b8d92ae6d51dea8f23fdd
Desarrollo Radiast <desarrolloradiast@gmail.com>
2025-12-08 19:27:35 -0400

    corrigiendo los prefijos para production y development en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
-const buildBase = '/proyectodemo/';
+const buildBase = '/proyectodemo/build/';
 
 export default defineConfig({
     base: process.env.NODE_ENV === 'production' ? buildBase : '/',
